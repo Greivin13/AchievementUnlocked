db3f00d72af0445f092fa020fd446a88bc9a2286
Austin1094 <Austin.Adams1094@gmail.com>
2023-05-31 15:49:30 -0400

    Added auth.js file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,5 @@
 const Sequelize = require('sequelize');
-<<<<<<< HEAD
 
-require('dotenv').config();
-
-// create connection to db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
-=======
 require('dotenv').config();
 
 let sequelize;
@@ -30,6 +18,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
->>>>>>> 5749beac8cfd201569b6b398347b9fa087e8cbca
 
 module.exports = sequelize;
