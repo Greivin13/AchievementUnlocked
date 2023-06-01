8a8677fd3b4c250bf93fa656f64f40b1933b3758
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-05-31 20:55:42 -0400

    did what I could in handlebars - needs testing after routing is done

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const path = require('path');
-const express = require('express');
-var session = require('express-session');
-const routes = require('./controllers');
-const sequelize = require('./config/connection.js');
+const path = require("path");
+const express = require("express");
+var session = require("express-session");
+const routes = require("./controllers");
+const sequelize = require("./config/connection.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: true }
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
