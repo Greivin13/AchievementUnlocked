e0c2cb39a4ceb961d6723c00137cd449d54acc73
Bwhitman33 <bwhitman33@gmail.com>
2023-06-04 13:40:49 -0400

    fixed id's in revComments seeds

--- seeds/revComments-seeds.js
+++ seeds/revComments-seeds.js
@@ -4,17 +4,23 @@ const revCommentData = [
     {
         title: 'Test_Title', 
         revComment_text: 'Test Review Text Goes Here Fuckers',
-        username: 'Rin'
+        username: 'Rin',
+        user_id: '2',
+        review_id: '1'
     },
     {
         title: 'Test_Title 2', 
         revComment_text: 'Test Review Text Goes Here Fuckers 2',
-        username: 'Rin'
+        username: 'Rin',
+        user_id: '2',
+        review_id: '2'
     },
     {
         title: 'Test_Title 3', 
         revComment_text: 'Test Review Text Goes Here Fuckers 3',
-        username: 'Rin'
+        username: 'Rin',
+        user_id: '2',
+        review_id: '3'
     }
 ]
 
