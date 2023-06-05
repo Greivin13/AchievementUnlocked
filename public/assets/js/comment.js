0baaf5611e5dbb05e06d62cab8c159414441230d
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-06-04 22:09:13 -0400

    uh maybe fixed stuff

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -1,40 +1,40 @@
-const fetch = require('node-fetch');
-// Function to add a  comment.
-async function addComment(postID,  userId, commentText){
-    try{
-        const response = await fetch("/api/comments-routes.js",{
-            method :"POST",
-            headers: {
-                "Content-Type":"application/json",
-            },
-            body: JSON.stringify({postId, userId, commentText}),
-        });
-        if (response.ok){
-            const newComment = await response.json();
-            console.log("New comment", newComment);
-        } else{
-            console.error("Failed to  add new  comment.");
-        }
-    } catch(error){
-        console.error("Error", error);
-    }
-}
-// Function to delete comment
-async function deleteComment(commentId){
-    try{
-        const response = await fetch("/api/comments-routes.js",{
-            method :"DELETE",
-        });
-        if (response.ok){
-            console.log( "Comment deleted", newComment);
-        } else{
-            console.error("Failed to delete comment.");
-        }
-    } catch(error){
-        console.error("Error", error);
-    }
-}
-module.exports = {
-    addComment,
-    deleteComment
-}
+// const fetch = require("node-fetch");
+// // Function to add a  comment.
+// async function addComment(postID, userId, commentText) {
+//   try {
+//     const response = await fetch("/api/comments-routes.js", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ postId, userId, commentText }),
+//     });
+//     if (response.ok) {
+//       const newComment = await response.json();
+//       console.log("New comment", newComment);
+//     } else {
+//       console.error("Failed to  add new  comment.");
+//     }
+//   } catch (error) {
+//     console.error("Error", error);
+//   }
+// }
+// // Function to delete comment
+// async function deleteComment(commentId) {
+//   try {
+//     const response = await fetch("/api/comments-routes.js", {
+//       method: "DELETE",
+//     });
+//     if (response.ok) {
+//       console.log("Comment deleted", newComment);
+//     } else {
+//       console.error("Failed to delete comment.");
+//     }
+//   } catch (error) {
+//     console.error("Error", error);
+//   }
+// }
+// module.exports = {
+//   addComment,
+//   deleteComment,
+// };
--- seeds/Review-seeds.js
+++ seeds/Review-seeds.js
@@ -1,25 +1,24 @@
-const { Review } = require('../models');
+const { Review } = require("../models");
 
 const reviewData = [
-    {
-        title: 'Test_Title', 
-        review_content: 'Test Review Text Goes Here Fuckers',
-        username: 'Prism1',
-        user_id: "3"
-    },
-    {
-        title: 'Test_Title 2', 
-        review_content: 'Test Review Text Goes Here Fuckers 2',
-        username: 'Prism1',
-        user_id: "3"
-    },
-    {
-        title: 'Test_Title 3', 
-        review_content: 'Test Review Text Goes Here Fuckers 3',
-        username: 'Prism1',
-        user_id: "3"
-    }
-]
+  {
+    title: "Test_Title",
+    review_content: "Test Review Text Goes Here Fuckers",
+    username: "Prism"
+  }
+  // {
+  //     title: 'Test_Title 2',
+  //     review_content: 'Test Review Text Goes Here Fuckers 2',
+  //     username: 'Prism1',
+  //     user_id: "3"
+  // },
+  // {
+  //     title: 'Test_Title 3',
+  //     review_content: 'Test Review Text Goes Here Fuckers 3',
+  //     username: 'Prism1',
+  //     user_id: "3"
+  // }
+];
 
 const seedReview = () => Review.bulkCreate(reviewData);
 
