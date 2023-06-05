6ccf53d1b63afbb413128af886c07f31c4f37ebd
Bwhitman33 <bwhitman33@gmail.com>
2023-06-04 20:32:56 -0400

    Got sign in and up routes working. Modified home routes to display db data for posts and reviews. Working on getting posts and reviews per unique session id for profile. almost there.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,37 +1,37 @@
-function GetPlayerSummaries() {
-  return new Promise(function (resolve, reject) {
-    $.ajax({
-      url: "/steamData",
-      type: "GET",
-      dataType: "json",
-      success: function (data) {
-        console.log("GetPlayerSummaries success");
-        displayPlayerSummary(data);
-        // $("#testDiv").html(JSON.stringify(data));
-        // insert data into handlebars template
-        // res.render("profile", { data: data });
-        resolve(data);
-        return data;
-      },
-      error: function (err) {
-        reject(err);
-      },
-    });
-  });
-}
+// function GetPlayerSummaries() {
+//   return new Promise(function (resolve, reject) {
+//     $.ajax({
+//       url: "/steamData",
+//       type: "GET",
+//       dataType: "json",
+//       success: function (data) {
+//         console.log("GetPlayerSummaries success");
+//         displayPlayerSummary(data);
+//         // $("#testDiv").html(JSON.stringify(data));
+//         // insert data into handlebars template
+//         // res.render("profile", { data: data });
+//         resolve(data);
+//         return data;
+//       },
+//       error: function (err) {
+//         reject(err);
+//       },
+//     });
+//   });
+// }
 
-async function displayPlayerSummary(data) {
-  router.get("/", (req, res) => {
-    res.render("homepage", data);
-  });
-}
+// async function displayPlayerSummary(data) {
+//   router.get("/", (req, res) => {
+//     res.render("homepage", data);
+//   });
+// }
 
-const thisPlayerSummary = GetPlayerSummaries(async (data) => {
-  const api_url = "/steamData";
-  const response = await fetch(api_url);
-  const playerData = await response.json();
-  console.log(playerData);
-});
+// const thisPlayerSummary = GetPlayerSummaries(async (data) => {
+//   const api_url = "/steamData";
+//   const response = await fetch(api_url);
+//   const playerData = await response.json();
+//   console.log(playerData);
+// });
 
 // app.get("/steamData", async (request, response) => {
 //   console.log(req.params);
