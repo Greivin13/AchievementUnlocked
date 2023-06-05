ca60ff16deafe63dc509a8f57b3d0ef71a9a46d6
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-06-05 16:42:25 -0400

    buttons all work

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -20,9 +20,7 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
+const signupFormHandler = async () => {
   //   const username = document.querySelector("#name-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
@@ -34,26 +32,39 @@ const signupFormHandler = async (event) => {
     if (password !== passwordConfirmation) {
       alert("Passwords do not match");
     } else {
-      const response = await fetch("/api/users", {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
+      // const response = await fetch("/api/users", {
+      //   method: "POST",
+      //   body: JSON.stringify({ email, password }),
+      //   headers: { "Content-Type": "application/json" },
+      // });
 
-      if (response.ok) {
-        document.location.replace("/");
-      } else {
-        alert(response.statusText);
-      }
+      // if (response.ok) {
+      //   document.location.replace("/");
+      // } else {
+      //   alert(response.statusText);
+      // }
+      let url = "http://localhost:3001/api/user/";
+
+      let options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: '{"username":"Rin2","email":"thisisfake@gmail.com","password":"pass123"}',
+      };
+
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.error("error:" + err));
     }
   }
 };
 
-document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
+// document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
 
 $("#signupBtn").click(function (e) {
   e.preventDefault();
-  document.location.replace("/sign-up");
+  signupFormHandler();
+  document.location.replace("/");
 });
 
 // $("#loginBtn").click(function (e) {
