ef82a374f5029210a614455065e68f5fe176995c
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-06-07 15:44:22 -0400

    still working with heroku

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "127.0.0.1",
+      host: "mysql://b05db9d19de748:4be79ac5@us-cdbr-east-06.cleardb.net/heroku_fe1f270e4a64d16?reconnect=true",
       dialect: "mysql",
       port: 3306,
     });
