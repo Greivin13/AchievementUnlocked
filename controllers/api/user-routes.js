c26e39024495094eceeb98f96f656b62a8d95f99
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-06-06 23:15:39 -0400

    Working version of updated app

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,27 +72,6 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-  try {
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((userData) => {
-        req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.username = userData.username;
-          req.session.loggedIn = true;
-        });
-        res.redirect(303, "/");
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
 
 router.post("/login", async (req, res) => {
@@ -152,3 +131,4 @@ router.put("/:id", (req, res) => {
 });
 
 module.exports = router;
+
