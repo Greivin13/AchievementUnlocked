0f1043d072ff93dde8b5fbacd9c54aa3a5afc1e3
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-06-06 20:38:55 -0400

    a couple changes to the home page

--- server.js
+++ server.js
@@ -68,27 +68,6 @@ sequelize.sync({ force: false }).then(() => {
 //     res.status(500).json({ error: 'An error occurred' });
 //   }
 // });
-app.get('/steamProfile', async (req, res) => {
-  const steamID = req.query.steamID;
-
-  try {
-    // Use the steamapi package to fetch the user profile data using the provided Steam ID
-    const playerData = await steam.getUserSummary(steamID);
-    
-    const userProfile = {
-      personaname: playerData.personaname,
-      avatarfull: playerData.avatarfull,
-      steamid: playerData.steamid,
-      profileurl: playerData.profileurl,
-      // Add more properties as needed
-    };
-
-    res.render("steamProfile", { userProfile });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
 
 
 app.get('/steamMembers', async (req, res) => {
@@ -96,24 +75,28 @@ app.get('/steamMembers', async (req, res) => {
   const steamID1 = '76561198054586238';
   const steamID2 = '76561198947331366';
   const steamID3 = '76561198289182228';
+  const steamID4 = '76561199227392699';
 
   try {
     const queryUrl1 = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID}`;
     const queryUrl2 = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID1}`;
     const queryUrl3 = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID2}`;
     const queryUrl4 = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID3}`;
+    const queryUrl5 = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID4}`;
     const apiResponse1 = await axios.get(queryUrl1);
     const apiResponse2 = await axios.get(queryUrl2);
     const apiResponse3 = await axios.get(queryUrl3);
     const apiResponse4 = await axios.get(queryUrl4);
+    const apiResponse5 = await axios.get(queryUrl5);
     const playerData1 = apiResponse1.data.response.players[0];
     const playerData2 = apiResponse2.data.response.players[0];
     const playerData3 = apiResponse3.data.response.players[0];
     const playerData4 = apiResponse4.data.response.players[0];
+    const playerData5 = apiResponse5.data.response.players[0];
 
     // Create an array and push the playerData objects
     const memberSummaries = [];
-    memberSummaries.push(playerData1, playerData2, playerData3, playerData4);
+    memberSummaries.push(playerData1, playerData2, playerData3, playerData4, playerData5);
 
     // Render the "steamMembers" view with the memberSummaries data
     res.render('steamMembers', { memberSummaries });
@@ -124,20 +107,20 @@ app.get('/steamMembers', async (req, res) => {
 });
 
 
-app.get("/steamData", async (request, response) => {
-  const steamID = '76561199036046793'; // Replace with the desired Steam ID
+// app.get("/steamData", async (request, response) => {
+//   const steamID = '76561199036046793'; // Replace with the desired Steam ID
 
-  try {
-    const queryUrl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID}`;
-    const apiResponse = await axios.get(queryUrl);
-    const playerData = apiResponse.data.response.players[0];
+//   try {
+//     const queryUrl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${steamID}`;
+//     const apiResponse = await axios.get(queryUrl);
+//     const playerData = apiResponse.data.response.players[0];
 
-    response.render("profile", { playerData });
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: 'An error occurred' });
-  }
-});
+//     response.render("profile", { playerData });
+//   } catch (error) {
+//     console.error(error);
+//     response.status(500).json({ error: 'An error occurred' });
+//   }
+// });
 
 
 steam.resolve('https://steamcommunity.com/profiles/76561199036046793/').then(id => {
